e39451b9356404194861122e2630c890201d706e
tu-turu <DREYTAZAVR@gmail.com>
2018-03-10 22:57:42 +0000

    Lab2 done! Progs swap.c & revert_string.c were improved

--- src/revert_string/revert_string.c
+++ src/revert_string/revert_string.c
@@ -1,7 +1,22 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int nextIndex = 0; // contains the index to be altered
+	int lastIndex = (int)strlen(str) - 1;
+	
+	// bubble. Ex: hello -> ohell -> olhhel -> ollhe -> olleh
+	while (nextIndex != lastIndex)
+	{
+		char temp = str[lastIndex];
+		
+		for (int i = lastIndex; i > nextIndex; i--)
+		{
+			str[i] = str[i - 1];
+		}
+		str[nextIndex] = temp;
+		nextIndex++;
+	}
 }
 
